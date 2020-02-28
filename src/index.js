22df84b89aaaccc6b7471ae04e3afcc348f85ee4
dizsart <darogadibia@gmail.com>
2020-02-28 02:39:32 +0100

    added all the neccessary files to the file structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css'; 
+import React, {useState, useEffect}  from "react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Products = () => {
+    const [products, setProducts] = useState({});
+   async function fetchData() {
+       const res = await fetch("http://localhost:8083/api/v1/view");
+       const data = await res.json()
+      setProducts(data);
+      
+   console.log(data);
+   }
+   useEffect(() => {
+   fetchData()
+}, []);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return (<div className = "container">
+        <h3 align = "center"> All Products</h3>
+        <div className = "table" align = "center" >
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Product Description</th>
+                    <th>Quantity Avaliable</th>
+                    <th>Quantity Sold</th>
+                </tr>
+                </thead>
+                <tbody>
+                    
+                    <tr key ={1}>
+                    <td>{JSON.stringify(products[0])}</td>
+                <td>{JSON.stringify(products)}</td>
+                <td>12</td>
+                <td>5</td>
+                </tr>
+                </tbody>
+            
+        </div>
+    </div>);
+    };
+
+
+
+
+ReactDOM.render(<Products/>, document.getElementById('root'));
